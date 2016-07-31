540219533f1f220cb0ebba1a376096b5536d4caa
Mark Apinis <mapinis2019@gmail.com>
2016-07-31 12:07:04 -0400

    Screwing Around, Episode 1

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,21 +1,36 @@
 var Observable = require("data/observable").Observable;
+var view = require("ui/core/view");
 
 function getMessage(counter) {
     if (counter <= 0) {
-        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
+        return "Hoorraaay! You clicked something 42 times! What a 1337 Hacker!";
     } else {
+        counter += Math.floor(Math.random() * 20)
         return counter + " taps left";
     }
+}
+
+function pageLoaded(args) {
+    var page = args.object;
+    var tabView = view.getViewByID(page, "buttonTabView");
+    tabView.selectedIndex = 1;
 }
 
 function createViewModel() {
     var viewModel = new Observable();
     viewModel.counter = 42;
+    viewModel.secondCounter = 0;
     viewModel.message = getMessage(viewModel.counter);
+    viewModel.secondMessage = "# of times that you have tapped the button: " + viewModel.secondCounter;
 
     viewModel.onTap = function() {
         this.counter--;
         this.set("message", getMessage(this.counter));
+    }
+
+    viewModel.onSecondButtonTap = function() {
+        this.secondCounter++;
+        this.set("secondMessage", "# of times that you have tapped the button:" + this.secondCounter);
     }
 
     return viewModel;
